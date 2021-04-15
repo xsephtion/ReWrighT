e7e3e8febb53c493d13aaf8d9135f879a1666b73
Gabriel Luis G. Borjal <gborjal01@gmail.com>
2021-04-16 01:13:09 +0800

    cors fix trial

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\corsMiddleware::class
+        
     ];
 
     /**
